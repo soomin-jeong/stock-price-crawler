8a19d34f20c2f85f072e715f7bd7c122f8df8f37
Moritz Walther <friedicecream1@gmail.com>
2021-03-27 21:33:08 +0100

    DCA fully functional
    only the stock data is missing. Waiting on debug.

--- trading_methodologies/__init__.py
+++ trading_methodologies/__init__.py
@@ -2,4 +2,4 @@ from .oneoff import oneoff
 from .DCA import DCA
 from .DCA import add_days
 from .rebalance import rebalance
-
+from .trading_util import write_as_csv
--- trading_methodologies/oneoff.py
+++ trading_methodologies/oneoff.py
@@ -13,7 +13,7 @@ def write_as_csv(data):
     #file_path = os.path.join('crawled_data', self.output_name, '.csv')
     with open(filename, 'w+', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(["Asset Alloc.", "Asset", "Amount($)", "Asset price", "# shares"])
+        writer.writerow(["Trading Method.", "Asset Alloc.", "Asset", "Amount($)", "Asset price", "# shares"])
         for x in data:
             writer.writerow(x)
 
