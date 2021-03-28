60f069ba8a449f13143fc4a8435225003ab948e8
Moritz Walther <friedicecream1@gmail.com>
2021-03-28 22:29:02 +0200

    more work on rebalance

--- trading_methodologies/rebalance.py
+++ trading_methodologies/rebalance.py
@@ -1,2 +1,74 @@
-def rebalance():
-    return 'rebalance'
+import csv
+import pandas as pd
+import datetime
+import trading_methodologies.trading_util as trading_util
+
+def rebalance(method, investment_period):
+    #load our CSV data files
+    portfoliodf = pd.read_csv('./portfolio_allocations.csv')
+    tradedatadf = pd.read_csv('./trading_methodologies.csv')
+    stocksdf = pd.read_csv('./amundi-msci-wrld-ae-c.csv')
+    cbondsdf = pd.read_csv('./ishares-global-corporate-bond-$.csv')
+    sbondsdf = pd.read_csv('./db-x-trackers-ii-global-sovereign-5.csv')
+    golddf = pd.read_csv('./spdr-gold-trust.csv')
+    cashdf = pd.read_csv('./usdollar.csv')
+
+    if method == "oneoff":
+        #load all trade data which was made with oneoff method
+        oneoff_trades_df = tradedatadf[tradedatadf['Trading Method.'] == "Oneoff"]
+        print("printing oneoff_trades_df dataframe")
+        print(oneoff_trades_df.head())
+
+        # Get a series of unique values in column 'portfolio ID' of the dataframe
+        #returns nparray object whcih we convert to list
+        x = oneoff_trades_df['Asset Alloc.'].unique()
+        portfolio_allocation_IDs = x.tolist()
+
+        print("printing portfolio_allocation_IDs list")
+        print(portfolio_allocation_IDs[:5])
+
+        for x in portfolio_allocation_IDs:
+            #get target allocation
+            #print(str(portfolio_allocation_IDs[x-1]))
+            print(portfolio_allocation_IDs[3])
+            #print(portfoliodf.head())
+            #target_alloc_df = portfoliodf.loc[portfoliodf['Asset Alloc.'] == 2]
+            target_alloc_df = portfoliodf.loc[portfoliodf['Asset Alloc.'] == int(portfolio_allocation_IDs[x-1])]
+            target_alloc_stocks = target_alloc_df.iloc[0]['ST']
+            target_alloc_cbonds = target_alloc_df.iloc[0]['CB']
+            target_alloc_sbonds = target_alloc_df.iloc[0]['PB']
+            target_alloc_gold = target_alloc_df.iloc[0]['GO']
+            target_alloc_cash = target_alloc_df.iloc[0]['CA']
+
+            #get trade amount for each allocation
+            initial_quant_stocks = tradedatadf.loc[(tradedatadf['Asset Alloc.'] == int(portfolio_allocation_IDs[x-1])) & (tradedatadf['Asset'] == "stocks")].iloc[0]['#']
+            print(initial_quant_stocks) 
+            initial_quant_cbonds = tradedatadf.loc[(tradedatadf['Asset Alloc.'] == int(portfolio_allocation_IDs[x-1])) & (tradedatadf['Asset'] == "cbonds")].iloc[0]['#']
+            initial_quant_sbonds = tradedatadf.loc[(tradedatadf['Asset Alloc.'] == int(portfolio_allocation_IDs[x-1])) & (tradedatadf['Asset'] == "sbonds")].iloc[0]['#']
+            initial_quant_gold = tradedatadf.loc[(tradedatadf['Asset Alloc.'] == int(portfolio_allocation_IDs[x-1])) & (tradedatadf['Asset'] == "gold")].iloc[0]['#']
+            initial_quant_cash = tradedatadf.loc[(tradedatadf['Asset Alloc.'] == int(portfolio_allocation_IDs[x-1])) & (tradedatadf['Asset'] == "cash")].iloc[0]['#']
+
+            #get initial investment date
+            initial_date = tradedatadf.loc[(tradedatadf['Asset Alloc.'] == int(portfolio_allocation_IDs[x-1])) & (tradedatadf['Asset'] == "cbonds")].iloc[0]['Date']
+            initial_date_obj = datetime.datetime.strptime(initial_date, '%d/%m/%Y')
+            #rebalance for the first time in the same month on the 15th
+            rebalance_initial_date = datetime.date(initial_date_obj.year, initial_date_obj.month, 15)
+            print(rebalance_initial_date)
+
+            #loop through investment period and rebalance
+            for x in range(0, int(investment_period)):
+                #print("test")
+                if initial_date_obj.day < 15:
+                    date_of_rebalance, price_of_rebalance_cbonds = trading_util.find_data_point("cbonds", trading_util.add_months(rebalance_initial_date, x))
+                    print(price_of_rebalance_cbonds)
+                    #if we have reached the end of our available data exit the loop
+                    if price_of_rebalance_cbonds == None:
+                        break
+                else:
+                    date_of_rebalance, price_of_rebalance_cbonds = trading_util.find_data_point("cbonds", trading_util.add_months(rebalance_initial_date, x+1))
+                
+                #date_of_rebalance, price_of_rebalance_stock = trading_util.find_data_point("stocks", trading_util.add_months(rebalance_initial_date, x))
+
+    elif method == "DCA":
+        print("DCA")
+    return 'rebalance succeeded'
--- trading_methodologies/trading_util.py
+++ trading_methodologies/trading_util.py
@@ -1,6 +1,10 @@
 import os
 import csv
-
+import time
+import datetime
+import pandas as pd
+import math
+import calendar
 
 def write_as_csv(data, write_mode): 
     filename = 'trading_methodologies.csv'  
@@ -8,13 +12,73 @@ def write_as_csv(data, write_mode):
     if write_mode == "overwrite":
         with open(filename, 'w+', newline='') as file:
             writer = csv.writer(file)
-            writer.writerow(["Trading Method.", "Asset Alloc.", "Asset", "Amount($)", "Asset price", "# shares"])
+            writer.writerow(["Date", "Trading Method.", "Purchase ID", "Asset Alloc.", "Asset", "Amount($)", "Asset price", "#"])
             for x in data:
                 writer.writerow(x)
     #or pass append if you just want to write to the end of the file
     elif write_mode == "append":
         with open(filename, 'a+', newline='') as file:
             writer = csv.writer(file)
-            writer.writerow(["Trading Method.", "Asset Alloc.", "Asset", "Amount($)", "Asset price", "# shares"])
+            writer.writerow(["Date", "Trading Method.", "Purchase ID", "Asset Alloc.", "Asset", "Amount($)", "Asset price", "#"])
             for x in data:
                 writer.writerow(x)
+
+#add months adds months while dealing with end of the month cases where 31/30th is given as days input
+def add_months(sourcedate, months):
+    month = sourcedate.month - 1 + months
+    year = sourcedate.year + month // 12
+    month = month % 12 + 1
+    day = min(sourcedate.day, calendar.monthrange(year,month)[1])
+    end_date = datetime.date(year, month, day)
+    return end_date
+
+#add days adds days while dealing with changes in months    
+def add_days(sourcedate, days_delta):
+    #date_1 = datetime.datetime.strptime(sourcedate, "%d/%m/%Y")
+    end_date = sourcedate + datetime.timedelta(days=days_delta)
+    return end_date
+
+def find_data_point(asset_class, sourcedate):
+    date_on_date = None
+    price_on_date = None
+    data_source = None
+    if (asset_class == "stocks"):
+        data_source = pd.read_csv('./amundi-msci-wrld-ae-c.csv')
+        print("loaded stock data")
+    elif (asset_class == "cbonds"):
+        data_source = pd.read_csv('./ishares-global-corporate-bond-$.csv')
+        print("loaded cbond data")
+    elif (asset_class == "sbonds"):
+        data_source = pd.read_csv('./db-x-trackers-ii-global-sovereign-5.csv')
+        print("loaded sbond data")
+    elif (asset_class == "gold"):
+        data_source = pd.read_csv('./spdr-gold-trust.csv')
+        print("loaded gold data")
+    elif (asset_class == "cash"):
+        data_source = pd.read_csv('./usdollar.csv')
+        print("loaded cash data")
+
+    try: 
+        info_for_date = data_source.loc[data_source['date'] == sourcedate.strftime('%d/%m/%Y')]
+        price_on_date =  info_for_date.iloc[0]['price']
+        date_on_date = sourcedate
+        print ("on the date " + sourcedate.strftime('%d/%m/%Y') + " the asset " + str(asset_class) + " cost " + str(price_on_date))
+    except: 
+        #because there is no trading on the weekend if our investment is on the weekend we might not get data so we will try 
+        #incrementing days till we arrive at monday 
+        print("there is no data for " + sourcedate.strftime('%d/%m/%Y' + " this date may fall on the weekend. We will try to get data for Monday"))
+        new_date = None
+        for i in range(1, 4):
+            try:
+                #becuase incrementing dates could pring us to the next month we call add days function
+                print("We incremented your date by:" + str(i))
+                new_date = add_days(sourcedate,i)
+                info_for_date = data_source.loc[data_source['date'] == new_date.strftime('%d/%m/%Y')]
+                price_on_date =  info_for_date.iloc[0]['price']
+                date_on_date = new_date
+                print ("The date you're looking for may be on the weekend. On the date " + new_date.strftime('%d/%m/%Y') + " the asset " + str(asset_class) + " cost " + str(price_on_date))
+                break
+            except:
+                print("there is no data for " + new_date.strftime('%d/%m/%Y'))
+
+    return date_on_date, price_on_date
