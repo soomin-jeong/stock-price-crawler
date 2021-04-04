9d9376c380d72e83a1929dc59311c417da26b32e
Moritz Walther <friedicecream1@gmail.com>
2021-04-04 23:20:18 +0200

    DCA rebalance work

--- trading_methodologies/DCA.py
+++ trading_methodologies/DCA.py
@@ -6,6 +6,7 @@ import pandas as pd
 import math
 import calendar
 import trading_methodologies.trading_util as trading_util
+import trading_methodologies.rebalance as previous_rebalance
 
 #add months adds months while dealing with end of the month cases where 31/30th is given as days input
 def add_months(sourcedate, months):
@@ -67,7 +68,7 @@ def find_data_point(asset_class, sourcedate):
 
     return date_on_date, price_on_date
 
-def DCA(startmoney, investment_date, investment_period):
+def DCA(startmoney, investment_date, investment_period, rebal):
 
     #floored division to find the amount to be invested per month
     money_per_month = math.floor(startmoney/investment_period)
@@ -105,30 +106,83 @@ def DCA(startmoney, investment_date, investment_period):
         gold_money = money_per_month*gold_percentage
         cash_money = money_per_month*cash_percentage
 
+        #initialize empty dateframe to store previous trades if we need to access for rebalancing 
+        columns =['ST_Q', 'ST_P', 'CB_Q', 'CB_P', 'SB_Q', 'SB_P', 'GO_Q', 'GO_P', 'CA_Q', 'CA_P', 'Money', 'Portf_Val']
+        previous_rebalance =  pd.DataFrame(columns = columns)
+
         #execute trade with portfolio logic for every month of investment period
         for x in range(0,investment_period):
-            print("iterator value is: " + str(x))
+            
+            print("DCA iterator value is: " + str(x))
+            stock_date, stock_price = find_data_point("stocks", add_months(date_obj, x))
             cbond_date, cbond_price = find_data_point("cbonds", add_months(date_obj, x))
             sbond_date, sbond_price = find_data_point("sbonds", add_months(date_obj, x))
             gold_date, gold_price = find_data_point("gold", add_months(date_obj, x))
             cash_date  = find_data_point("cash", add_months(date_obj, x))[0]
+            DCA_date = stock_date
             cash_price = 1
-            stock_date, stock_price = find_data_point("stocks", add_months(date_obj, x))
-
 
             #calculate units we can buy
-            #stock_units = math.floor(stock_money/stock_price)
+            stock_units = math.floor(stock_money/stock_price)
             cbond_units = math.floor(cbond_money/cbond_price)
             sbond_units = math.floor(sbond_money/sbond_price)
             gold_units = math.floor(gold_money/gold_price)
             cash_units = math.floor(cash_money/cash_price)
             
             #add data to array for later csv writing
-            #data.append(tuple(["DCA", portf_alloc, stock_money, stock_price, stock_units]))
+            data.append(tuple(["DCA", portf_alloc, stock_money, stock_price, stock_units]))
             data.append(tuple(["DCA", portf_alloc, cbond_money, cbond_price, cbond_units]))
             data.append(tuple(["DCA", portf_alloc, sbond_money, sbond_price, sbond_units]))
             data.append(tuple(["DCA", portf_alloc, gold_money, gold_price, gold_units]))
             data.append(tuple(["DCA", portf_alloc, cash_money, cash_price, cash_units]))
+            
+            #if we want to rebalance then we need to make sure to rebalance the whole portfolio
+            if rebal == "TRUE":
+                #add the current DCA trade to DF 
+                print("entered if block for rebalance")
+                trade_port_value = stock_units*stock_price + cbond_units*cbond_price + sbond_price*sbond_units + gold_units*gold_price + cash_units*cash_price
+                trade_data = {
+                    'ST_Q': stock_units,
+                    'ST_P': stock_price,
+                    'CB_Q': cbond_units,
+                    'CB_P': cbond_price,
+                    'SB_Q': sbond_units,
+                    'SB_P': sbond_price,
+                    'GO_Q': gold_units,
+                    'GO_P': gold_price,
+                    'CA_Q': cash_units,
+                    'CA_P': cash_price,
+                    'Money': 0,
+                    'Portf_Val': trade_port_value
+                    }
+
+                previous_rebalance = previous_rebalance.append(trade_data, ignore_index=True)
+
+                #The previous rebalance will be the sum of all previous trades in portfolio alloc so drop all the other trade except the last
+                print("The dataframe length is" + str(len(previous_rebalance)))
+                if len(previous_rebalance) == 4:
+                    previous_rebalance = previous_rebalance.drop(previous_rebalance.index[0])
+
+                #Sum of the columns
+                Rebal_ST_Q = previous_rebalance['ST_Q'].sum()
+                #Rebal_ST_P = previous_rebalance['ST_P'].sum()
+                Rebal_CB_Q = previous_rebalance['CB_Q'].sum()
+                #Rebal_CB_P = previous_rebalance['CB_P'].sum()
+                Rebal_SB_Q = previous_rebalance['SB_Q'].sum()
+                #Rebal_SB_P = previous_rebalance['SB_P'].sum()
+                Rebal_GO_Q = previous_rebalance['GO_Q'].sum()
+                #Rebal_GO_P = previous_rebalance['GO_P'].sum()
+                Rebal_CA_Q = previous_rebalance['CA_Q'].sum()
+                #Rebal_CA_P = previous_rebalance['CA_P'].sum()
+                Rebal_Money = previous_rebalance['Money'].sum()
+                Rebal_Portf_Val = previous_rebalance['Portf_Val'].sum()
+                print("The sum of the portfolio value is " + str(Rebal_Portf_Val))
+                
+                #Execute rebalance
+                rebalance.DCA_rebalance(Rebal_ST_Q, Rebal_CB_Q, Rebal_SB_Q, Rebal_GO_Q, Rebal_CA_Q, stock_percentage, cbond_percentage, sbond_percentage, gold_percentage, cash_percentage, Rebal_Money, DCA_date)
+                #Write to dataframe
+
+                #Add to data list to be written to CSV
 
         trading_util.write_as_csv(data, "append")
     return 'DCA has succeeded'
