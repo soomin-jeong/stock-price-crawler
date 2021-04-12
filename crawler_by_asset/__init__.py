d43faf363e65043164654db851723d4dffe8f164
Soo Min Jeong <soominjeongkr@gmail.com>
2021-04-12 13:07:10 +0200

    refactored the crawler runners

--- crawler_by_asset/__init__.py
+++ crawler_by_asset/__init__.py
@@ -4,3 +4,5 @@ from .gold import gold_crawler
 from .soverign_bond import sbond_crawler
 from .global_stocks import stock_crawler
 from .corporate_bonds import cbond_crawler
+
+crawlers = [cash_crawler, gold_crawler, sbond_crawler, stock_crawler, cbond_crawler]
