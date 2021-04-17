d00d3e43e6554217695d01c85825e3a9288dfd65
Soo Min Jeong <soominjeongkr@gmail.com>
2021-04-17 17:08:44 +0200

    added the performance metric csv

--- main.py
+++ main.py
@@ -14,26 +14,26 @@ from trading_methodologies import oneoff, DCA, rebalance, oneoff_rebalance
 # portfolio = portfolio_generator.generate_allocation()
 # portfolio_generator.write_as_csv(portfolio)
 
-#run trading methodology
-#one-off
+# #run trading methodology
+# #one-off
 # data, message = oneoff(10000, '01/03/2020')
 # print(message)
-
-
-#one-off rebalanced
+#
+#
+# #one-off rebalanced
 # oneoff_rebalance(3)
-#DCA
-#print(DCA(10000, '03/02/2020', 3, "FALSE"))
-
-
-#DCA rebalanced
+# # DCA
+# print(DCA(10000, '03/02/2020', 3, "FALSE"))
+#
+#
+# #DCA rebalanced
 # print(DCA(10000, '01/03/2020', 2, "TRUE"))
 trading_methodology_filename = 'trading_methodologies.csv'
 
 
 # Performance Analysis
-data = pd.read_csv(trading_methodology_filename, header=0, parse_dates=['Date'])
-pa = PerformanceAnalyst(data)
+aggr_data = pd.read_csv(trading_methodology_filename, header=0, parse_dates=['Date'])
+pa = PerformanceAnalyst(aggr_data)
 pa.run_performance_analysis()
 
 
