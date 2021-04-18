d58fa569e7abbaad7f181563ccf106758a7f7d53
Moritz Walther <friedicecream1@gmail.com>
2021-04-18 16:45:04 +0200

    for lena

--- main.py
+++ main.py
@@ -29,12 +29,12 @@ data, message = oneoff(100000, '01/01/2020', 1)
 print(message)
 
 #one-off rebalanced
-#oneoff_rebalance(1)
+oneoff_rebalance(1)
 
 #DCA
 #setting the flase flag here means we are not rebalancing
-#print(DCA(100000, '01/01/2020', 1, "FALSE"))
-#print ("regular DCA finished")
+print(DCA(100000, '01/01/2020', 1, "FALSE"))
+print ("regular DCA finished")
 
 #DCA rebalanced
 #setting the true flag means we will rebalance
@@ -45,20 +45,20 @@ print ("DCA rebal finished")
 #uncomment these lines to run all trades for 3 month investment period
 
 #one-off
-#data, message = oneoff(100000, '01/01/2020', 3)
-#print(message)
+data, message = oneoff(100000, '01/01/2020', 3)
+print(message)
 
 #one-off rebalanced
-#oneoff_rebalance(3)
+oneoff_rebalance(3)
 
 #DCA
 #setting the flase flag here means we are not rebalancing
-#print(DCA(100000, '01/01/2020', 3, "FALSE"))
+print(DCA(100000, '01/01/2020', 3, "FALSE"))
 
 
 #DCA rebalanced
 #setting the true flag means we will rebalance
-#print(DCA(100000, '01/01/2020', 3, "TRUE"))
+print(DCA(100000, '01/01/2020', 3, "TRUE"))
 
 #*******************************************
 #Uncomment these lines to run analysis of the trades
